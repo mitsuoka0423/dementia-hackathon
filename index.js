1365434472f291d39d4391a3df7ddb62b30156aa
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-08-05 15:22:17 +0900

    feat: img

--- index.js
+++ index.js
@@ -159,6 +159,14 @@ const Page = () => {
 
   return (
     <div>
+      <div>
+        <img src="./img/help-mark.png" width="100%"></img>
+        <div className="uk-text-large">
+          <div className="uk-text-bold ">私は若年性認知症です</div>
+          <div>皆さんの協力を必要としています</div>
+        </div>
+      </div>
+
       <TextArea label="聞き取り結果" value={speechList.join("\n")} height={"300px"} disabled></TextArea>
 
       {
