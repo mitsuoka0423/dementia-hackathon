edab11532dcc8f5d389eea8e35df339d19fee721
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-08-06 14:01:15 +0900

    chore: text style

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -7,15 +7,15 @@ const MainLayout = ({ children, setRoute, routes }) => {
       <nav className="navbar">
         <button onClick={() => { setRoute(routes.HELP_MARK) }} className="navbar-item navbar-button">
           <img src="./img/help.png" className="navbar-button-img"></img>
-          <div>ヘルプマーク</div>
+          <div className="navbar-button-text">ヘルプマーク</div>
         </button>
         <button onClick={() => { setRoute(routes.PROFILE) }} className="navbar-item navbar-button">
           <img src="./img/profile.png" className="navbar-button-img"></img>
-          <div>プロフィール</div>
+          <div className="navbar-button-text">プロフィール</div>
         </button>
         <button onClick={() => { setRoute(routes.TIMELINE) }} className="navbar-item navbar-button">
           <img src="./img/clock.png" className="navbar-button-img"></img>
-          <div>タイムライン</div>
+          <div className="navbar-button-text">タイムライン</div>
         </button>
       </nav>
     </div>
