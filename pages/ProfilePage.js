f1135deeb76b625f70b414a937d82f040514c8ae
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-08-06 12:37:42 +0900

    chore: upd picture

--- pages/ProfilePage.js
+++ pages/ProfilePage.js
@@ -1,14 +1,14 @@
 const ProfilePage = () => {
   return (
     <div className="profile-page">
-      <img src="./img/profile.png" className="profile-page-img"></img>
+      <img src="./img/sample.png" className="profile-page-img"></img>
       <div className="uk-text-large">はかた　いちろう</div>
-      <div>
+      <div className="uk-text-large">
         2020年に若年性アルツハイマーになりました。
         覚えることが苦手で、10分前のことを忘れてしまいます。
         お手伝いをお願いできると助かります。
       </div>
-      <div>
+      <div className="uk-text-large">
         困っていたら家族に連絡をお願いします。
         TEL: XXX-XXXX-XXXX
       </div>
