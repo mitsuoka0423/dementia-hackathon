ef5aba5060f244aaf5ae165212181c3efe38998c
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-08-06 09:22:27 +0900

    feat: add nav

--- layouts/NavbarLayout.js
+++ layouts/NavbarLayout.js
@@ -1,14 +1,14 @@
-const NavbarLayout = ({ setRoutes, routes, children }) => {
+const NavbarLayout = ({ children, setRoute, routes }) => {
   return (
     <div>
       <div className="uk-container">
         {children}
       </div>
       <nav className="navbar">
-        <button onClick={() => { setRoutes(routes.TOP) }}>TOP</button>
-        <button onClick={() => { setRoutes(routes.RECORDING) }}>記録</button>
-        <button onClick={() => { setRoutes(routes.HELP_MARK) }}>ヘルプマーク</button>
-        <button onClick={() => { setRoutes(routes.HELP_MARK) }}>プロフィール</button>
+        <button onClick={() => { setRoute(routes.TOP) }}>TOP</button>
+        <button onClick={() => { setRoute(routes.RECORDING) }}>記録</button>
+        <button onClick={() => { setRoute(routes.HELP_MARK) }}>ヘルプマーク</button>
+        <button onClick={() => { setRoute(routes.PROFILE) }}>プロフィール</button>
       </nav>
     </div>
   );
---
+++ pages/ProfilePage.js
@@ -0,0 +1,10 @@
+const ProfilePage = () => {
+  return (
+    <div>
+      <div>プロフィール</div>
+      <button>記録する</button>
+      <button>振り返る</button>
+      <button>自分のこと</button>
+    </div>
+  );
+};
